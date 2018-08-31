073cadd2175563faa7c821e18a606d100daa3c57
Bijay Das <imbijaydas@gmail.com>
2018-08-31 10:54:09 +0530

    Added the you don't have any todos portion

--- src/components/App.js
+++ src/components/App.js
@@ -2,71 +2,83 @@ import React from 'react';
 import NavBar from './NavBar';
 import TodoItems from './TodoItems';
 
-
 class App extends React.Component {
+
     constructor() {
         super();
         this.state = {
             addTodoValue: '',
             todoList: [
-                {
-                    id: 1,
-                    todo: 'Making coffee'
-                },
-                {
-                    id: 2,
-                    todo: 'Go for a walk'
-                },
-                {
-                    id: 3,
-                    todo: 'Buy an iPad'
-                },
-                {
-                    id: 4,
-                    todo: 'Move to Paris'
-                },
-                {
-                    id: 5,
-                    todo: 'Move to Chzech Republic'
-                },
-
+                // {
+                //     id: 1,
+                //     todo: 'Making coffee'
+                // },
+                // {
+                //     id: 2,
+                //     todo: 'Go for a walk'
+                // },
+                // {
+                //     id: 3,
+                //     todo: 'Buy an iPad'
+                // },
+                // {
+                //     id: 4,
+                //     todo: 'Move to Paris'
+                // },
+                // {
+                //     id: 5,
+                //     todo: 'Move to Chzech Republic'
+                // }
             ]
         };
 
         this.updateTodoInputValue = this.updateTodoInputValue.bind(this);
         this.checkForSubmit       = this.checkForSubmit.bind(this);
+        this.handleDelete         = this.handleDelete.bind(this);
     }
 
-    updateTodoInputValue( e ) {
+    updateTodoInputValue(e) {
         this.setState({ addTodoValue: e.target.value });
     }
 
-    checkForSubmit( e ) {
+    checkForSubmit(e) {
         if ( e.key == 'Enter' ) {
 
             // Adding current Todo value from state
             let todo = this.state.addTodoValue;
-
             // Clear the input box
             this.setState({addTodoValue: ''});
 
             // Copying existing TodoList
             let todoList = this.state.todoList;
 
-            // Fetching the last id from list and adding 1 with it
-            let lastID = this.state.todoList[this.state.todoList.length - 1].id;
-            lastID++;
+            // Initializing the lastID with 1
+            let lastID = 1;
+
+            if (todoList.length > 0) {
+                // If there are some todos present in the array then
+                // Fetching the last id from list and adding 1 with it
+                lastID = this.state.todoList[this.state.todoList.length - 1].id;
+                lastID++;
+            }
 
             // Pushing the new todo value from state to the copied todo list
             todoList.push({id: lastID, todo: todo});
 
             // Finally adding the updated arr to the state todoList
             this.setState({todoList: todoList});
-
         }
     }
 
+    handleDelete(idToRemove) {
+        let NewTodoList = this.state.todoList.filter(data => {
+            if ( data.id != idToRemove ) return data;
+        });
+        this.setState({ todoList: NewTodoList });
+    }
+
     render() {
+
         return(
             <main>
                 <NavBar/>
@@ -82,7 +94,7 @@ class App extends React.Component {
                     </div>
 
                     <div className="todo-list-container">
-                        <TodoItems todo={this.state.todoList}/>
+                        <TodoItems handleDelete={this.handleDelete} todo={this.state.todoList}/>
                     </div>
                 </section>
             </main>
--- src/components/TodoItems.js
+++ src/components/TodoItems.js
@@ -1,18 +1,38 @@
 import React from 'react';
 
-const TodoItems = ( props ) => {
-    const li = props.todo.map(( key, value ) => {
+const TodoItems = ({ todo, handleDelete }) => {
+
+    // If some todos are available in the state then display the todos
+    // Else display you don't have any todos left
+
+    if ( todo.length > 0 ) {
+
+        const lis = todo.map((data) => {
+            return(
+                <li key={data.id}>
+                {data.todo}
+                <i
+                onClick={
+                    () => {
+                        handleDelete(data.id)
+                    }
+                }
+                className="far fa-trash-alt"></i>
+                </li>
+            );
+        });
         return(
-            <li key={key.id}>
-                {key.todo}
-                <i className="far fa-trash-alt"></i>
-            </li>
+            <ul>
+                {lis}
+            </ul>
         );
-    });
-    return(
-        <ul>
-            {li}
-        </ul>
-    );
+    }
+    else {
+        return(
+            <div className="well">
+                You dont have any todos left
+            </div>
+        );
+    }
 }
 export default TodoItems;
